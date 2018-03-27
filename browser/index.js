2c67a7b2bade3bd2fb2ee5d02ed1e3099a672dfc
Meng Wang <wangmeng_hb@163.com>
2018-03-27 11:26:18 +0800

    project init

---
+++ browser/index.js
@@ -0,0 +1,36 @@
+
+
+// Browser environment sniffing
+export const inBrowser = typeof window !== 'undefined'
+export const inWeex = typeof WXEnvironment !== 'undefined' && !!WXEnvironment.platform
+export const weexPlatform = inWeex && WXEnvironment.platform.toLowerCase()
+export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
+export const isIE = UA && /msie|trident/.test(UA)
+export const isIE9 = UA && UA.indexOf('msie 9.0') > 0
+export const isEdge = UA && UA.indexOf('edge/') > 0
+export const isAndroid = (UA && UA.indexOf('android') > 0) || (weexPlatform === 'android')
+export const isIOS = (UA && /iphone|ipad|ipod|ios/.test(UA)) || (weexPlatform === 'ios')
+export const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge
+
+
+export let supportsPassive = false
+if (inBrowser) {
+  try {
+    const opts = {}
+    Object.defineProperty(opts, 'passive', ({
+      get () {
+        /* istanbul ignore next */
+        supportsPassive = true
+      }
+    })
+    window.addEventListener('test-passive', null, opts)
+  } catch (e) {}
+}
+
+// Use our detect's results. 
+// passive applied if supported, capture will be false either way.
+elem.addEventListener(
+  'touchstart',
+  fn,
+  supportsPassive ? { passive: true } : false
+);
