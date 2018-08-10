1d22a2e9040725cbc3e7d977354c24e848780e7e
Meng Wang <wangmeng_hb@163.com>
2018-08-10 14:28:23 +0800

    fix error

--- browser/index.js
+++ browser/index.js
@@ -2,35 +2,28 @@
 
 // Browser environment sniffing
 export const inBrowser = typeof window !== 'undefined'
-export const inWeex = typeof WXEnvironment !== 'undefined' && !!WXEnvironment.platform
-export const weexPlatform = inWeex && WXEnvironment.platform.toLowerCase()
 export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
 export const isIE = UA && /msie|trident/.test(UA)
 export const isIE9 = UA && UA.indexOf('msie 9.0') > 0
 export const isEdge = UA && UA.indexOf('edge/') > 0
-export const isAndroid = (UA && UA.indexOf('android') > 0) || (weexPlatform === 'android')
-export const isIOS = (UA && /iphone|ipad|ipod|ios/.test(UA)) || (weexPlatform === 'ios')
+export const isAndroid = (UA && UA.indexOf('android') > 0)
+export const isIOS = (UA && /iphone|ipad|ipod|ios/.test(UA))
 export const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge
 
-
+// detect if browser support passive event 
+// which is used to improve scoll behavior in mobile device
 export let supportsPassive = false
 if (inBrowser) {
   try {
     const opts = {}
     Object.defineProperty(opts, 'passive', ({
       get () {
-        /* istanbul ignore next */
         supportsPassive = true
       }
-    })
+    }))  
     window.addEventListener('test-passive', null, opts)
+  // The above expression is NOT aync, and event name can be anything else 
+  // If browser supports passive event, it will try to get opts's passive property
+  // Otherwise, it will treat opts as boolean.  so the getter of opts are never touched 
   } catch (e) {}
 }
-
-// Use our detect's results. 
-// passive applied if supported, capture will be false either way.
-elem.addEventListener(
-  'touchstart',
-  fn,
-  supportsPassive ? { passive: true } : false
-);
