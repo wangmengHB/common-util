c6f67655659eff05e62dec21a9b54ae10757d758
meng.wm <meng.wm@alibaba-inc.com>
2019-06-20 09:46:03 +0800

    add utils

---
+++ src/base/objects.ts
@@ -0,0 +1,202 @@
+import { isObject, isUndefinedOrNull } from './types';
+
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+export function deepClone<T>(obj: T): T {
+  if (!obj || typeof obj !== 'object') {
+    return obj;
+  }
+  if (obj instanceof RegExp) {
+    return obj as any;
+  }
+  const result: any = Array.isArray(obj)? []: {};
+  Object.keys(obj as any).forEach((key: string) => {
+		if ((<any>obj)[key] && typeof (<any>obj)[key] === 'object') {
+			result[key] = deepClone((<any>obj)[key]);
+		} else {
+			result[key] = (<any>obj)[key];
+		}
+	});
+  return result;
+}
+
+export function deepFreeze<T>(obj: T): T {
+  if (!obj || typeof obj !== 'object') {
+    return obj;
+  }
+  const stack: any[] = [obj];
+  while(stack.length > 0) {
+    const obj = stack.shift();
+    Object.freeze(obj);
+    for (const key in obj) {
+      if (hasOwnProperty.call(obj, key)) {
+        const prop = obj[key];
+        if (typeof prop === 'object' && !Object.isFrozen(prop)) {
+          stack.push(prop);
+        }
+      }
+    }
+  }
+  return obj;
+}
+
+export function cloneAndChange(obj: any, changer: (orig: any) => any ): any {
+  return _cloneAndChange(obj, changer, new Set());
+}
+
+function _cloneAndChange(obj: any, changer: (orig: any) => any, seen: Set<any>): any {
+  if (isUndefinedOrNull(obj)) {
+    return obj;
+  }
+  const changed = changer(obj);
+  if (typeof changed !== 'undefined') {
+    return changed;
+  }
+
+  if (Array.isArray(obj)) {
+    const r1: any[] = [];
+    for (const e of obj) {
+      r1.push(_cloneAndChange(e, changer, seen));
+    }
+    return r1;
+  }
+
+  if (isObject(obj)) {
+    if (seen.has(obj)) {
+      throw new Error('Cannot clone recursive data-structure');
+    }
+    seen.add(obj);
+    const r2 = {};
+    for (let i2 in obj) {
+      if (hasOwnProperty.call(obj, i2)) {
+        (<any>r2)[i2] = _cloneAndChange(obj[i2], changer, seen);
+      }
+    }
+    seen.delete(obj);
+    return r2; 
+  }
+
+  return obj;
+}
+
+/**
+ * Copies all properties of source into destination. The optional parameter "overwrite" allows to control
+ * if existing properties on the destination should be overwritten or not. Defaults to true (overwrite).
+ */
+export function mixin(destination: any, source: any, overwrite: boolean = true): any {
+  if (!isObject(destination)) {
+    return source;
+  }
+  if (isObject(source)) {
+    Object.keys(source).forEach(key => {
+      if (key in destination) {
+        if (overwrite) {
+          if (isObject(destination[key]) && isObject(source[key])) {
+            mixin(destination[key], source[key], overwrite);
+          } else {
+            destination[key] = source[key];
+          }
+        } else {
+          // do nothing
+        }
+      } else {
+        destination[key] = source[key];
+      }
+    })
+  }
+  return destination;
+}
+
+export function assign(destination: any, ...sources: any[]): any {
+  sources.forEach(source => source && Object.keys(source).forEach(key => destination[key] = source[key]));
+  return destination;
+}
+
+export function equals(one: any, other: any): boolean {
+  if (one === other) {
+    return true;
+  }
+  if (one === null || one === undefined || other === null || other === undefined) {
+    return false;
+  }
+  if (typeof one !== typeof other) {
+    return false;
+  }
+  if (Array.isArray(one) !== Array.isArray(other)) {
+    return false;
+  }
+  if (typeof one !== 'object') {
+    return false;
+  }
+
+  if (Array.isArray(one)) {
+    if (one.length !== other.length) {
+      return false;
+    }
+    for (let i = 0; i < one.length; i++) {
+      if (!equals(one[i], other[i])) {
+        return false;
+      }
+    }
+  } else {
+    const oneKeys: string[] = [];
+    for (const key in one) {
+      oneKeys.push(key);
+    }
+    oneKeys.sort();
+    const otherKeys: string[] = [];
+    // 这种写法比较了原型链上的属性，如果使用 Object.keys(), 则只比较自己的属性
+    for (const key in other) {
+      otherKeys.push(key);
+    }
+    otherKeys.sort();
+    if (!equals(oneKeys, otherKeys)) {
+      return false;
+    }
+    for (let i = 0; i < oneKeys.length; i++) {
+      if (!equals(one[oneKeys[i]], other[oneKeys[i]])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+export function safeStringify(obj: any): string {
+  const seen: any[] = [];
+  return JSON.stringify(obj, (key, value) => {
+    if (isObject(value) || Array.isArray(value)) {
+      if (seen.indexOf(value) !== -1) {
+        return '[Circular]';
+      } else {
+        seen.push(value);
+      }
+    }
+    return value;
+  });
+}
+
+export function getOrDefault<T, R>(obj: T, fn: (obj: T) => R | undefined, defaultValue: R): R {
+  const result = fn(obj);
+  return typeof result === 'undefined'? defaultValue: result;
+}
+
+type obj = { [key: string]: any;};
+
+export function distinct(base: obj, target: obj): obj {
+  const result = Object.create(null);
+  if (!base || !target) {
+    return result;
+  }
+  const targetKeys = Object.keys(target);
+  targetKeys.forEach(k => {
+    const baseValue = base[k];
+    const targetValue = target[k];
+    if (!equals(baseValue, targetValue)) {
+      result[k] = targetValue;
+    }
+  })
+
+  return result;
+}
+
--- src/base/types.ts
+++ src/base/types.ts
@@ -6,3 +6,117 @@ const _typeof = {
   function: 'function'
 };
 
+export function isString(str: any): str is string {
+  if (typeof str === _typeof.string) {
+    return true;
+  }
+  return false;
+}
+
+export function isNumber(obj: any): obj is number {
+  if (typeof obj === _typeof.number && !isNaN(obj)) {
+    return true;
+  }
+  return false;
+}
+
+export function isBoolean(obj: any): obj is boolean {
+  return obj === true || obj === false;
+}
+
+export function isUndefined(obj: any): obj is undefined {
+  return typeof obj === _typeof.undefined;
+}
+
+export function isUndefinedOrNull(obj: any): obj is undefined | null {
+  return isUndefined(obj) || obj === null;
+}
+
+export function isFunction(obj: any): obj is Function {
+  return typeof obj === _typeof.function;
+}
+
+export function areFunctions(...objects: any[]): boolean {
+  return objects.length > 0 && objects.every(isFunction);
+}
+
+export function isStringArray(value: any): value is string[] {
+  return Array.isArray(value) && (<any[]>value).every(str => isString(str));
+}
+
+export function isObject(obj: any): obj is Object {
+  return typeof obj === _typeof.object
+    && obj !== null
+    && !Array.isArray(obj)
+    && !(obj instanceof RegExp)
+    && !(obj instanceof Date);
+}
+
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+export function isEmptyObject(obj: any): obj is any {
+  if (!isObject(obj)) {
+    return false;
+  }
+  for (let key in obj) {
+    if (hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export type TypeConstraint = string | Function;
+
+export function validateConstraint(arg: any, constraint: TypeConstraint | undefined): void {
+  if (isString(constraint)) {
+    if (typeof arg !== constraint) {
+      throw new Error(`argument does not match constraint: typeof ${constraint}`)
+    }
+  } else if (isFunction(constraint)) {
+    try {
+      if (arg instanceof constraint) {
+        return;
+      }
+    } catch {
+
+    }
+    // 字面值类型走这个判断
+    if (!isUndefinedOrNull(arg) && arg.constructor === constraint) {
+      return;
+    }
+    if (constraint.length === 1 && constraint.call(undefined, arg) === true) {
+      return;
+    }
+    throw new Error(`argument does not match one of these constraints: arg instanceof constraint, arg.constructor === constraint, nor constraint(arg) === true`);
+  }
+}
+
+export function validateConstraints(args: any[], constraints: Array<TypeConstraint | undefined>): void {
+  const len = Math.min(args.length, constraints.length);
+  if (args.length !== constraints.length) {
+    throw new Error('validateConstraints: args.length not equal constraints.length');
+  }
+  for (let i = 0; i < len; i++) {
+    validateConstraint(args[i], constraints[i]);
+  }
+}
+
+export function withNullAsUndefined<T>(x: T | null): T | undefined {
+  return x === null? undefined: x;
+}
+
+export function withUndefinedAsNull<T>(x: T | undefined): T | null {
+  return isUndefined(x)? null: x;
+}
+
+// 这个方法有缺陷，只能返回原型链上的属性名称 ！！！
+export function getAllPropertyNames(obj: object): string[] {
+  let res: string[] = [];
+  let proto = Object.getPrototypeOf(obj);
+  while(Object.prototype !== proto) {
+    res = res.concat(Object.getOwnPropertyNames(proto));
+    proto = Object.getPrototypeOf(proto);
+  }
+  return res;
+}
